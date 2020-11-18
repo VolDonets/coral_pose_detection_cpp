82448d67c59f7db432bbfca41b7e95e788db9daf
VolDonets <vovan.s.marsa@gmail.com>
2020-11-18 21:17:34 +0200

    Added a waiting for starting initialization.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,12 @@
 // Created by biba_bo on 2020-11-16.
 //
 
+#include <zconf.h>
 #include "process_camera_stream/gst_pipeline_processor.h"
 
 int main(int argv, char *argc[]) {
+    std::cout << "Prepare for initing!\n";
+    sleep(2);
+    std::cout << "End of preparing!\n";
     return start_gst_loop();
 }
